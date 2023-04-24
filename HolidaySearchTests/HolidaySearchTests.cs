3dc575989c4f50768c264a5223e63c568025613a
Jake Heaney <jake@yappy.com>
2023-04-24 19:10:51 +0100

    data model files and init tests

---
+++ HolidaySearchTests/HolidaySearchTests.cs
@@ -0,0 +1,32 @@
+namespace HolidaySearchTests
+{
+    public class HolidaySearchTests
+    {
+        [Fact]
+        public void InvalidSearchObject_ThrowsError()
+        {
+
+        }     
+        
+        //No matching departure/arrival airport - still return holidays with date change and duration?
+        [Fact]
+        public void NoHolidaysMatchingSearch_ReturnsWarning()
+        {
+
+        }     
+        
+        [Fact]
+        public void WhereSearchHasOneMatchingResult_SingleResultShouldBeDisplayed()
+        {
+
+        }
+
+        [Fact]
+        public void WhereSearchHasMultipleMatchingResults_BestValueResultShouldBeDisplayed()
+        {
+
+        }
+
+        
+    }
+}
